497b263d5e3ecb78492822a75ed879c0d59625c4
FarmerChillax <farmerchillax@outlook.com>
2024-07-03 18:29:00 +0800

    chore: update signature.go

--- signature.go
+++ signature.go
@@ -1,10 +1,6 @@
 package agollo
 
 import (
-	"crypto/hmac"
-	"crypto/sha1"
-	"encoding/base64"
-	neturl "net/url"
 	"strconv"
 	"time"
 )
@@ -19,20 +15,10 @@ func newSignature(appId, accesskeySecret string) *signature {
 }
 
 func (s *signature) getAuthorization(url, timestamp string) string {
-	sign := timestamp + signDelimiter + s.getURL2PathWithQuery(url)
-	key := []byte(s.AccesskeySecret)
-	mac := hmac.New(sha1.New, key)
-	mac.Write([]byte(sign))
-	ss := mac.Sum(nil)
-	return base64.StdEncoding.EncodeToString(ss)
+	return s.AccesskeySecret
 }
 
 func (s *signature) getTimestamp() string {
 	t := time.Now().UnixNano() / 1e6
 	return strconv.Itoa(int(t))
-}
-
-func (s *signature) getURL2PathWithQuery(rawurl string) string {
-	url, _ := neturl.Parse(rawurl)
-	return url.RequestURI()
 }
